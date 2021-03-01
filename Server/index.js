c8bcceafcbf8c2365b0fbd851fcc0db968ea71f5
DAVID PURTILL <G00374622@gmit.ie>
2021-03-01 23:37:43 +0000

    Connected Express to mongo database

---
+++ Server/index.js
@@ -0,0 +1,61 @@
+// imports
+let express = require('express');
+let cors = require('cors')
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+let app = express();
+
+// using the body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// connection to mongoDB Cloud.
+mongoose.connect('mongodb+srv://david:admin@profprac.vgaki.mongodb.net/Users?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+let Schema = mongoose.Schema;
+
+// creating the basic schema, will update 
+let UserSchema = new Schema({
+    users: {
+        fName: String,
+        age: Number
+    } 
+});
+// var for schema to be used
+var User = mongoose.model('user', UserSchema);
+
+const connection = mongoose.connection;
+const port = 3000;
+
+//opening the the connection to db
+connection.once('open', () => {
+    console.log("Mongo Db has been connected");
+});
+
+// just a basic hello world get req
+app.get('/', (req, res) => {
+    res.send("Hello");
+});
+//getting the users back from the db
+app.get('/adduser', (req,res)=>{
+    User.find({}, (err,users)=>{
+        res.json(users);
+    })
+});
+//posting users to the database
+app.post('/adduser', (req,res)=>{
+   let users = new User(req.body);
+   users.save()
+   .then(item =>{
+    res.send("item saved to database");
+   }).catch(err=>{
+    res.status(400).send("unable to save to database");
+   });
+   
+});
+// opening the port for 3000
+app.listen(port, () => {
+    console.log("Listening on port 3000...");
+});
+
+//module.exports = {User};
