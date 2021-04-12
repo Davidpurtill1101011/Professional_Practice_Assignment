c284c517011750a8a736264271895022339ca255
DAVID PURTILL <G00374622@gmit.ie>
2021-04-12 22:51:06 +0100

    Added more personal details to the database

--- Server/index.js
+++ Server/index.js
@@ -18,9 +18,10 @@ let Schema = mongoose.Schema;
 
 // creating the basic schema, will update 
 let UserSchema = new Schema({
-    
-        fName: String,
-    
+    fName: String,
+    sName: String,
+    age: Number,
+    preferedExercise: String,
 });
 // var for schema to be used
 var User = mongoose.model('user', UserSchema);
@@ -43,25 +44,36 @@ app.get('/users', (req, res) => {
    res.sendFile(path.join(__dirname, '../src/Components/users/users.component.html'));
 });
 
+// //login Component
+// app.get('/login', (req,res)=>{
+//     res.sendFile(path.join(__dirname, '../src/Components/login-page/login-page.component.html'));
+// });
+
 //getting the users back from the db
-app.get('/adduser', (req,res)=>{
+app.get('/user', (req,res)=>{
     User.find({}, (err,users)=>{
         res.send(users);
     })
 });
+
 //posting users to the database
-app.post('/adduser', (req,res)=>{
+app.post('/user', (req,res)=>{
+   // creating the instance of the user
    let user = new User();
+   //setting the data that is entered to the database
    user.fName = req.body.fName;
+   user.sName = req.body.sName;
+   user.age = req.body.age;
+   user.preferedExercise = req.body.preferExe;
+   //savinf to the DB
    user.save()
    .then(item =>{
     res.send("Saved to database" );
-
    }).catch(err=>{
     res.status(400).send("unable to save to database");
-   });
-   
+   });// end of anon function
 });
+
 // opening the port for 3000
 app.listen(port, () => {
     console.log("Listening on port 3000...");
